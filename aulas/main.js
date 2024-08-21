0426e8a2f8b48b0763ef9ff0c288ba64aac608db
eliaCoelho <ellianee@hotmail.com>
2024-08-21 17:05:29 -0300

    incluído conteúdo arquivo main pasta aulas

--- aulas/main.js
+++ aulas/main.js
@@ -5,3 +5,24 @@ console.log(`Helo World`);
 console.log(typeof(1.5));
 console.log(typeof('3'));
 
+//operadores aritméticos
+console.log(2 + 4);
+console.log(2 * 4);
+console.log(2 - 4);
+console.log(2 / 4);
+console.log(2 % 4);
+console.log(2 ** 4);
+
+//incremento e decremento
+let count = 1;
+let step = 2;
+
+console.log(++count);
+
+console.log(--count);
+
+count += step;
+console.log(count);
+
+
+
